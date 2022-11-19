005395b0c45d9fe1d85428bdfa1fa268ed5e45da
Wilbert <74440630+wilbertbl@users.noreply.github.com>
2022-11-19 15:13:06 +0700

    upload

---
+++ sisfoGAUPH/resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+<!--
+//Nama : MahesYaputra
+//StudentID : 03081200047 
+-->
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+         <li class="nav-item">
+          <a href="/" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Home
+            </p>
+          </a>
+        </li>
+         <li class="nav-item">
+          <a href="/admin/categories" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Category
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/user-role" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              User Role
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/missing-item-status" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Missing Item Status
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/missing-item-status" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Reservation Status
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/schedule" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Schedule
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/attendance" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Attendance
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/reservation" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Reservation
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="/admin/reservation" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Pengajuan Replacement Class
+            </p>
+          </a>
+        </li>
+  </ul>
